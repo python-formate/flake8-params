3eb9c518765fc34659fb613b99eb386c35d8976a
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-12-21 09:07:47 +0000

    Remove pytest skip.

--- tests/test_flake8_params.py
+++ tests/test_flake8_params.py
@@ -1,10 +1,8 @@
 # stdlib
 import ast
-import sys
 from typing import List
 
 # 3rd party
-import pytest
 from coincidence.regressions import AdvancedFileRegressionFixture
 from domdf_python_tools.paths import PathPlus
 
@@ -18,7 +16,6 @@ def results(s: str) -> List[str]:
 	return ["{}:{}: {}".format(*r) for r in Plugin(ast.parse(s)).run()]
 
 
-@pytest.mark.skipif(sys.version_info >= (3, 12), reason="Line numbers are offset on earlier versions")
 def test_plugin(advanced_file_regression: AdvancedFileRegressionFixture):
 	res = results(example_code)
 	advanced_file_regression.check('\n'.join(res))
